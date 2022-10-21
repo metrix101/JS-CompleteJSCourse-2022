4127d6b3c3a5aae99118975e4f8ca79cfc2dca1d
Morgan <metrix2018@gmail.com>
2022-10-21 15:38:30 +0100

    IF/ESLE statement continue

--- fundamentals-2/challenge.js
+++ fundamentals-2/challenge.js
@@ -1,34 +1,37 @@
-// let dolphineScore = (96 + 108 + 89) / 3;
-// let koalasScore = (88 + 91 + 110) / 3;
+let dolphineScore = (96 + 108 + 89) / 3;
+let koalasScore = (88 + 91 + 110) / 3;
 
-// // if (dolphineScore > koalasScore) {
-// //     console.log(`DolphiN Wins`)
-// // } else {
-// //     console.log(`Koalas Won`)
-// // }
-
-// let teamAvg = (dolphin, koalas) => {
-//     dolphin = dolphineScore;
-//     koalas = koalasScore;
-
-//     if (dolphin > koalas) {
-//         console.log(`Dolphins Are the winners with the Score of ${dolphineScore}`)
-//     }
-//     if (dolphin < koalas) {
-//         console.log(`Koalas Won with ${koalasScore} Points.`)
-//     }
-//     else if (dolphin == koalas) {
-//         console.log(`This Scores are tied. Hence, there's no wiiner. Dolphins Point is ${dolphineScore}
-//         and Koalas Score Point is ${koalasScore}`)
-//     }
+// if (dolphineScore > koalasScore) {
+//     console.log(`DolphiN Wins`)
+// } else {
+//     console.log(`Koalas Won`)
 // }
-// teamAvg();
+
+let teamAvg = (dolphin, koalas) => {
+    dolphin = dolphineScore;
+    koalas = koalasScore;
+
+    if (dolphin > koalas) {
+        console.log(`Dolphins Are the winners with the Score of ${dolphineScore}`)
+    }
+    if (dolphin < koalas) {
+        console.log(`Koalas Won with ${koalasScore} Points.`)
+    }
+    else if (dolphin == koalas) {
+        console.log(`This Scores are tied. Hence, there's no wiiner. Dolphins Point is ${dolphineScore}
+        and Koalas Score Point is ${koalasScore}`)
+    }
+}
+teamAvg();
 
 const requirement = 100;
 let dolphinAvg = (97 + 112 + 101) / 3;
 let koalasAvg = (109 + 95 + 123) / 3;
 
-let winner = (dolphinAvg, koalasAvg) => {
+
+let winner = (dolAvg, koaAvg) => {
+    dolAvg = dolphinAvg;
+    koaAvg = koalasAvg;
     if (dolphinAvg > koalasAvg && dolphinAvg > requirement) {
         console.log(`Dolphin is the Winner with ${dolphinAvg} Points`)
     }
@@ -41,3 +44,18 @@ let winner = (dolphinAvg, koalasAvg) => {
 }
 
 winner();
+
+let testWinner = (dolWins, koaWins) => {
+    dolWins = (97 + 112 + 101) / 3;
+    koaWins = (109 + 95 + 106) / 3;
+    if (dolWins > koaWins && dolWins > 100) {
+        console.log(`Dolphins Won the Trophy with ${dolWins} Points`)
+    }
+    if (koaWins > dolWins && koaWins > 100) {
+        console.log(`Koalas Won the Trophy with ${koaWins} Points`)
+    }
+    else if (koaWins == dolWins && koaWins || dolWins > 100) {
+        console.log(`No Winner, Points are Dolphin ${dolWins} & Koalas ${koaWins}`)
+    }
+}
+testWinner();

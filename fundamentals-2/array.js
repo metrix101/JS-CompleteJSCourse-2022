031afe76ebdf8636f1d4781aa1548a7a940698d8
Morgan <metrix2018@gmail.com>
2022-10-29 16:02:47 +0100

    Array methods

---
+++ fundamentals-2/array.js
@@ -0,0 +1,19 @@
+const friends = ['Steven', 'John', 'Elvis', 'Paul'];
+// console.log(friends[1]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1])
+
+// friends[3] = 'Gabriel';
+
+// console.log(friends);
+
+const bio = ['David', 'Morgan', age = 2022 - 1996, 'Programmer',
+    'Aspiring Product Manager', friends];
+//console.log(bio);
+const newFriends = friends.push('Samson');
+//console.log(friends);
+
+//friends.pop([1]);
+
+console.log(friends.indexOf('Paul'));
+console.log(friends.include('Paul'));
